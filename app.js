ca4b35acd1a7f16b05ffd74aca384d2cd4a7377f
shubha <shubhampatidar961@gmial.com>
2023-03-02 20:49:05 +0530

    nextcommit

--- app.js
+++ app.js
@@ -2,17 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Nav from "./componets/header";
 import Body from "./componets/Body";
+import Footer from "./componets/footer";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./componets/About";
+import Error from "./componets/Error";
+import Contact from "./componets/Contact";
+import Details from "./componets/Restodetails";
 const App = () => {
   return (
     <>
-      {/*navigation bar
-       * nav items */
-      /*body *restocart *restiinfo *restocart *rating *cusin  */
-      /* footer*/}
       <Nav />
-      <Body />
+      {/* outlet help in nesting an router  */}
+      <Outlet />
+      <Footer />
     </>
   );
 };
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restodetales/:id",
+        element: <Details />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={Router} />);
